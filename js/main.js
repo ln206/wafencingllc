059f96b6d380655a93473f0b6f123366175dcb60
Long <longnguyen@tecace.com>
2017-11-20 16:00:46 -0800

    Add hero image, and scroll animation.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top - 50 //no need of parseInt here
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+// var shiftWindow = function() { scrollBy(0, -50) };
+// if (location.hash) shiftWindow();
+// window.addEventListener("hashchange", shiftWindow);
 
 var gallery_api;
 jQuery(document).ready(function(){ 
